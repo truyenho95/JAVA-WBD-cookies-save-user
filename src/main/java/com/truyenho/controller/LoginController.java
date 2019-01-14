3823e167cd16bf021b3bbb856d7ae310321ac9a8
truyenho95 <truyenho.webdev@gmail.com>
2019-01-14 13:28:26 +0700

    user set cookie feature

--- src/main/java/com/truyenho/controller/LoginController.java
+++ src/main/java/com/truyenho/controller/LoginController.java
@@ -2,13 +2,12 @@ package com.truyenho.controller;
 
 import com.truyenho.model.User;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CookieValue;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 @SessionAttributes("user")
@@ -24,6 +23,39 @@ public class LoginController {
     Cookie cookie = new Cookie("setUser", setUser);
     ModelAndView modelAndView = new ModelAndView("login");
     modelAndView.addObject("cookieValue", cookie);
+    return modelAndView;
+  }
+
+  @PostMapping("/dologin")
+  public ModelAndView doLogin(@ModelAttribute("user") User user, @CookieValue(value = "setUser", defaultValue = "") String setUser, HttpServletRequest request, HttpServletResponse response) {
+    ModelAndView modelAndView = new ModelAndView("login");
+    if (user.getEmail().equals("admin1@gmail.com") && user.getPassword().equals("12345")) {
+      if (user.getEmail() != null) {
+        setUser = user.getEmail();
+      }
+
+      Cookie cookie = new Cookie("setUser", setUser);
+      cookie.setMaxAge(24 * 60 * 60);
+      response.addCookie(cookie);
+
+      Cookie[] cookies = request.getCookies();
+      for (Cookie ck : cookies) {
+        if (ck.getName().equals("setUser")) {
+          modelAndView.addObject("cookieValue", ck);
+          break;
+        } else {
+          ck.setValue("");
+          modelAndView.addObject("cookieValue", ck);
+          break;
+        }
+      }
+      modelAndView.addObject("message", "Login success. Welcome ");
+    } else {
+      user.setEmail("");
+      Cookie cookie = new Cookie("setUser", setUser);
+      modelAndView.addObject("cookieValue", cookie);
+      modelAndView.addObject("message", "Login failed. Try again.");
+    }
     return modelAndView;
   }
 
